a1dd222cf88e1de02959340fe4ecba4c3a59ba5f
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-07-21 15:54:02 -0300

    Adiciona base dos testes de Questao e Alternativa

---
+++ tests/GeradorDeProvas.Testes.Unidade/Modulos/ModuloQuestao/QuestaoTests.cs
@@ -0,0 +1,101 @@
+namespace GeradorDeProvas.Testes.Unidade.Modulos.ModuloQuestao;
+
+[TestClass]
+public sealed class QuestaoTests
+{
+    [TestMethod]
+    public void Construtor_DeveVincular_CadaAlternativa_AQuestao()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_SemEnunciado_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_SemMateria_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_SemAlternativas_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_ComPoucasAlternativas_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_ComMuitasAlternativas_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_SemAlternativaCorreta_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+
+    [TestMethod]
+    public void Validar_ComMuitasAlternativasCorreta_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+
+        // Ação
+
+        // Asserção
+
+        throw new NotImplementedException("Teste não implementado");
+    }
+}
