b549e122a0f52b3619ce585b3b6c9fc0793f48f8
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-07-22 15:26:06 -0300

    Adiciona testes para Provas utilizando TDD

---
+++ src/GeradorDeProvas.Dominio/Modulos/ModuloProva/Prova.cs
@@ -0,0 +1,70 @@
+using GeradorDeProvas.Dominio.Compartilhado;
+using GeradorDeProvas.Dominio.Compartilhado.Identity;
+using GeradorDeProvas.Dominio.Modulos.ModuloDisciplina;
+using GeradorDeProvas.Dominio.Modulos.ModuloMateria;
+using GeradorDeProvas.Dominio.Modulos.ModuloQuestao;
+
+namespace GeradorDeProvas.Dominio.Modulos.ModuloProva;
+
+// título, disciplina, matéria*, série e quantidade de questões
+public sealed class Prova : EntidadeBase<Prova>, IEntidadeDoUsuario
+{
+    public string Titulo { get; set; } = string.Empty;
+    public Disciplina Disciplina { get; set; } = null!;
+    public Materia? Materia { get; set; }
+    public int Serie { get; set; }
+    public int QuantidadeQuestoes { get; set; }
+    public bool ProvaRecuperacao { get; set; }
+    public List<Questao> Questoes { get; set; } = [];
+    public Guid UserId { get; set; }
+
+    public Prova()
+    {
+    }
+
+    public Prova(
+        string titulo,
+        Disciplina disciplina,
+        Materia? materia,
+        int serie,
+        int quantidadeQuestoes,
+        bool provaRecuperacao,
+        List<Questao>? questoes = null
+    ) : this()
+    {
+        Titulo = titulo;
+        Disciplina = disciplina;
+        Materia = materia;
+        Serie = serie;
+        QuantidadeQuestoes = quantidadeQuestoes;
+        ProvaRecuperacao = provaRecuperacao;
+        Questoes = questoes ?? [];
+    }
+
+    public override List<string> Validar()
+    {
+        List<string> erros = [];
+
+        if (string.IsNullOrWhiteSpace(Titulo) || Titulo.Length < 2 || Titulo.Length > 100)
+            erros.Add("O campo \"Título\" deve ser conter entre 2 e 100 caracteres.");
+
+        if (Disciplina is null)
+            erros.Add("O campo \"Disciplina\" deve ser preenchido.");
+
+        if (Serie <= 0)
+            erros.Add("O campo \"Série\" deve ser maior que zero.");
+
+        if (ProvaRecuperacao && Materia is not null)
+            erros.Add("O campo \"Matéria\" não pode ser prenchido em uma prova de recuperação.");
+
+        else if (!ProvaRecuperacao && Materia is not null && !Equals(Materia.Serie, Serie))
+            erros.Add("O campo \"Série\" precisa alinhar com a série da \"Matéria\".");
+
+        return erros;
+    }
+
+    public override void Atualizar(Prova entidadeAtualizada)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/GeradorDeProvas.Testes.Unidade/Modulos/ModuloProva/ProvaTests.cs
@@ -0,0 +1,149 @@
+using GeradorDeProvas.Dominio.Modulos.ModuloDisciplina;
+using GeradorDeProvas.Dominio.Modulos.ModuloMateria;
+using GeradorDeProvas.Dominio.Modulos.ModuloProva;
+using Microsoft.Testing.Platform.Extensions.Messages;
+
+namespace GeradorDeProvas.Testes.Unidade.Modulos.ModuloProva;
+
+[TestClass]
+public sealed class ProvaTests
+{
+    [TestMethod]
+    public void Validar_SemTitulo_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Disciplina disciplina = new Disciplina("Matemática");
+        Materia materia = new Materia("Álgebra", 8, disciplina);
+
+        Prova prova = new Prova(string.Empty, disciplina, materia, 8, 0, false);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Título\" deve ser conter entre 2 e 100 caracteres.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_SemDisciplina_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Materia materia = new Materia("Álgebra", 8, null!);
+
+        Prova prova = new Prova("Prova de Álgebra 8a Serie", null!, materia, 8, 0, false);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Disciplina\" deve ser preenchido.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComSerieZeroOuAbaixo_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Disciplina disciplina = new Disciplina("Matemática");
+        Materia materia = new Materia("Álgebra", 0, disciplina);
+
+        Prova prova = new Prova("Prova de Álgebra", disciplina, materia, 0, 0, false);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Série\" deve ser maior que zero.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_ComSerieEMateria_Diferentes_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Disciplina disciplina = new Disciplina("Matemática");
+        Materia materia = new Materia("Álgebra", 8, disciplina);
+
+        Prova prova = new Prova("Prova de Álgebra", disciplina, materia, 5, 0, false);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Série\" precisa alinhar com a série da \"Matéria\".",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_RecuperacaoComMateria_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Disciplina disciplina = new Disciplina("Matemática");
+        Materia materia = new Materia("Álgebra", 8, disciplina);
+
+        Prova prova = new Prova("Prova de Álgebra", disciplina, materia, 5, 0, true);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Matéria\" não pode ser prenchido em uma prova de recuperação.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_QuantidadeQuestoesAbaixoDeUm_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Disciplina disciplina = new Disciplina("Matemática");
+        Materia materia = new Materia("Álgebra", 8, disciplina);
+
+        Prova prova = new Prova("Prova de Álgebra", disciplina, materia, 5, 0, true);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O campo \"Quantidade de Questões\" não pode ser zero ou negativo.",
+            erros.First()
+        );
+    }
+
+    [TestMethod]
+    public void Validar_MateriaFora_DaDisciplina_DeveRetornar_ErroCorrespondente()
+    {
+        // Arranjo
+        Disciplina disciplina = new Disciplina("Matemática");
+        Materia materia = new Materia("Álgebra", 8, disciplina);
+
+        Prova prova = new Prova("Prova de Álgebra", disciplina, materia, 5, 0, true);
+
+        // Ação
+        List<string> erros = prova.Validar();
+
+        // Asserção
+        Assert.HasCount(1, erros);
+        Assert.AreEqual(
+            "O valor do campo \"Matéria\" deve pertencer à \"Disciplina\" selecionada.",
+            erros.First()
+        );
+    }
+}
