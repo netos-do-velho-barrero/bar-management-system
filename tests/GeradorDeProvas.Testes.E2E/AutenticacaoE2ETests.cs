d7e216b07fee265a54320bc15a1250349c4914eb
Tiago Santini <66257666+tiagosantini@users.noreply.github.com>
2026-08-03 15:44:36 -0300

    Adiciona Playwright com testes básicos

---
+++ tests/GeradorDeProvas.Testes.E2E/AutenticacaoE2ETests.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.MSTest;
+
+namespace GeradorDeProvas.Testes.E2E;
+
+[TestClass]
+public sealed class AutenticacaoE2ETests : PageTest
+{
+    private string urlBase = "http://localhost:8001";
+
+    [TestMethod]
+    public async Task Deve_Exibir_TelaDeLogin_ParaUsuarioAnonimo()
+    {
+        // Arrange
+        // Act
+        await Page.GotoAsync($"{urlBase}/");
+
+        // Assert
+        await Expect(Page).ToHaveTitleAsync(new Regex("Entrar"));
+    }
+
+    [TestMethod]
+    public async Task Deve_RegistrarEAutenticar_Usuario()
+    {
+        // Arrange
+        const string email = "novo.usuario@teste.local";
+        const string senha = "Senha123!";
+
+        await Page.GotoAsync($"{urlBase}/Autenticacao/Registrar");
+
+        // Act
+        await Page.GetByLabel("E-mail").FillAsync(email);
+        await Page.GetByLabel("Senha", new() { Exact = true }).FillAsync(senha);
+        await Page.GetByLabel("Confirmar Senha").FillAsync(senha);
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Criar Conta" }).ClickAsync();
+
+        // Assert
+        string rotaAbsoluta = new Uri(Page.Url).AbsolutePath;
+
+        Assert.AreEqual("/", rotaAbsoluta);
+    }
+}
